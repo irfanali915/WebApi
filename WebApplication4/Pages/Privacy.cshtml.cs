ad0176c171659a9784d12461b0edc2b1fab05b71
Irfan Ali <irfan.ali@onsolve.com>
2020-01-25 00:26:00 +0530

    Update of name space

--- WebApplication4/Pages/Privacy.cshtml.cs
+++ WebApplication4/Pages/Privacy.cshtml.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
-namespace WebApplication4.Pages
+namespace NAGP.Sample.CoreApp.Pages
 {
     public class PrivacyModel : PageModel
     {
